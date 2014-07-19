6e9109cf0b909adc1891efd4349132eed18b1b0c
László ÁSHIN <laszlo@ashin.hu>
2014-07-19 09:50:31 +0200

    remove directory "obsolete" with its content

--- obsolete/openglstub.h
+++
@@ -1,6 +0,0 @@
-#ifndef _OPENGLSTUB_H
-#define _OPENGLSTUB_H 1
-
-extern int init_opengl_api();
-
-#endif /* _OPENGLSTUB_H */
--- obsolete/spring.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SPRING_H
-#define _SPRING_H 1
-
-void springEnable();
-void springDisable();
-
-#endif /* _SPRING_H */
