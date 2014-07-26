931aa13a5b224d72f7bed0afd1529160d82c8b53
László ÁSHIN <laszlo@ashin.hu>
2014-07-26 14:07:27 +0200

    bsp2: implement split statistics

--- editor/bsp2.cc
+++ editor/bsp2.cc
@@ -6,6 +6,11 @@
 #include <memory>
 #include <algorithm>
 #include <functional>
+#include <ostream>
+#include <limits>
+#include <stdexcept>
+#include <numeric>
+#include <iostream>
 
 namespace bsp {
 
@@ -20,6 +25,37 @@ public:
 private:
 	float x_, y_;
 };
+
+class Plane2d {
+public:
+	Plane2d() : a_(), b_(), c_() {}
+	Plane2d(const Vertex& v1, const Vertex& v2)
+	: a_(v2.y() - v1.y())
+	, b_(v1.x() - v2.x())
+	, c_(-(a_ * v1.x() + b_ * v1.y()))
+	{}
+
+	float determine(const Vertex& v) const { return a_ * v.x() + b_ * v.y() + c_; }
+	float dot(const Vertex& v) const { return a_ * v.y() - b_ * v.x(); }
+	void save(FILE* fp) const { fwrite(&a_, sizeof(a_), 1, fp); fwrite(&b_, sizeof(b_), 1, fp); fwrite(&c_, sizeof(c_), 1, fp); }
+	void print(std::ostream& os) const { os << "Plane2d a=" << a_ << " b=" << b_ << " c=" << c_ << std::endl; }
+	friend Vertex intersect(const Plane2d& lhs, const Plane2d& rhs);
+
+private:
+	float a_, b_, c_;
+};
+
+Vertex
+intersect(const Plane2d& lhs, const Plane2d& rhs)
+{
+	const float e = std::numeric_limits<float>::epsilon();
+	const float q = rhs.a_ * lhs.b_ - lhs.a_ * rhs.b_;
+	if (q > -e && q < e) throw std::overflow_error("parallel planes do not intersect each other");
+	const float p = 1.0f / q;
+	const float x = (rhs.b_ * lhs.c_ - lhs.b_ * rhs.c_) * p;
+	const float y = (lhs.a_ * rhs.c_ - rhs.a_ * lhs.c_) * p;
+	return Vertex(x, y);
+}
 
 class Wall {
 public:
@@ -41,6 +77,25 @@ struct Renderer {
 	void operator()(const Sector& s);
 };
 
+class SplitStats {
+public:
+	SplitStats() : balance_(), splitCount_() {}
+
+	void wallIsFront() { ++balance_; }
+	void wallIsBack() { --balance_; }
+	void wallIsSplit() { ++splitCount_; }
+
+	int balance() const { return balance_; }
+	size_t splitCount() const { return splitCount_; }
+
+	SplitStats& operator+=(const SplitStats& rhs) { balance_ += rhs.balance(); splitCount_ += rhs.splitCount(); return *this; }
+	void print(std::ostream& os) const { os << "balance=" << balance() << " splitCount=" << splitCount() << std::endl; }
+
+private:
+	int balance_;
+	size_t splitCount_;
+};
+
 class Sector {
 	typedef std::vector<Wall> Walls;
 
@@ -55,31 +110,61 @@ public:
 
 	int id() const { return id_; }
 	void add(const Wall& w) { walls_.push_back(w); }
+	SplitStats computeStats(const Plane2d& plane) const;
 
 private:
 	int id_;
 	Walls walls_;
 };
 
+SplitStats Sector::computeStats(const Plane2d& plane) const {
+	SplitStats result;
+	for (const_iterator i(begin()); i != end(); ++i) {
+		const float e = std::numeric_limits<float>::epsilon();
+		const float deta = plane.determine(i->a());
+		const float detb = plane.determine(i->b());
+		const int da = (deta > e) ? 1 : ((deta < -e) ? -1 : 0);
+		const int db = (detb > e) ? 1 : ((detb < -e) ? -1 : 0);
+		if (da * db < 0) {
+			result.wallIsSplit();
+		} else if (da + db > 0) {
+			result.wallIsFront();
+		} else {
+			result.wallIsBack();
+		}
+	}
+	return result;
+}
+
 void
 Renderer::operator()(const Sector& s)
 {
-    grSetColor((s.id() * 343) | 3);
+//	grSetColor((s.id() * 343) | 3);
+	grSetColor((rand() * 343) | 3);
 	std::for_each(s.begin(), s.end(), *this);
 }
 
 class Node {
 public:
-	void build() {}
+	void build();
 	void show() const { std::for_each(sectors_.begin(), sectors_.end(), Renderer()); }
 	void save(FILE*) const {}
 	void add(int sectorId, const Wall& w);
+	SplitStats computeStats(const Plane2d& plane) const;
 
 private:
 	typedef std::vector<Sector> Sectors;
 
 	Sectors sectors_;
 };
+
+SplitStats Node::computeStats(const Plane2d& plane) const {
+	SplitStats result;
+	for (Sectors::const_iterator i(sectors_.begin()); i != sectors_.end(); ++i) {
+		result += i->computeStats(plane);
+	}
+	return result;
+}
 
 void
 Node::add(int sectorId, const Wall& w)
@@ -88,6 +173,18 @@ Node::add(int sectorId, const Wall& w)
 		sectors_.push_back(Sector(sectorId));
 	}
 	sectors_.back().add(w);
+}
+
+void
+Node::build()
+{
+	for (Sectors::const_iterator i(sectors_.begin()); i != sectors_.end(); ++i) {
+		for (Sector::const_iterator j(i->begin()); j != i->end(); ++j) {
+			const Plane2d plane(j->a(), j->b());
+			SplitStats stats = computeStats(plane);
+			stats.print(std::cout);
+		}
+	}
 }
 
 class Tree {
