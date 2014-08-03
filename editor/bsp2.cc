bbf3ca08e8d6ced210ac2f0bfea30b7909bfd9a6
László ÁSHIN <laszlo@ashin.hu>
2014-08-03 23:17:23 +0200

    editor: implement saving bsp2

--- editor/bsp2.cc
+++ editor/bsp2.cc
@@ -13,6 +13,8 @@
 #include <iostream>
 #include <iterator>
 #include <map>
+#include <cmath>
+#include <cassert>
 
 namespace bsp {
 
@@ -52,6 +54,15 @@ inline bool operator<(const Vertex& lhs, Vertex d) {
 
 inline bool operator!=(const Vertex& lhs, const Vertex& rhs) { return !(lhs == rhs); }
 
+typedef std::vector<Vertex> Vertexes;
+
+void save(FILE* fp, const Vertex& v) {
+	short x = round(v.x());
+	short y = round(v.y());
+	fwrite(&x, sizeof(x), 1, fp);
+	fwrite(&y, sizeof(y), 1, fp);
+}
+
 class Plane2d {
 public:
 	Plane2d() : a_(), b_(), c_() {}
@@ -88,16 +99,38 @@ intersect(const Plane2d& lhs, const Plane2d& rhs)
 
 class Wall {
 public:
-	Wall(const Vertex& a, const Vertex& b) : a_(a), b_(b) {}
+	Wall(const Vertex& a, const Vertex& b, int tex) : a_(a), b_(b), tex_(tex) {}
 
 	Vertex a() const { return a_; }
 	Vertex b() const { return b_; }
+	int tex() const { return tex_; }
+
+	void save(FILE* fp, const Vertexes& vs) const;
 
 private:
 	Vertex a_, b_;
+	int tex_;
 };
 
 std::ostream& operator<<(std::ostream& os, const Wall& w) { return os << "Wall(" << w.a() << ", " << w.b() << ")"; }
+
+int getVertexIndex(const Vertexes& vs, const Vertex& v) {
+	Vertexes::const_iterator i(std::find(vs.begin(), vs.end(), v));
+	assert(i != vs.end());
+	return std::distance(vs.begin(), i);
+}
+
+void Wall::save(FILE* fp, const Vertexes& vs) const {
+	unsigned char buf[2 * 2 + 1 + 3 * 2 + 4], *p = buf;
+	*(short *)p = getVertexIndex(vs, a());
+	*(short *)(p + 2) = getVertexIndex(vs, b());
+	p[4] = 0;
+	*(unsigned short *)(p + 5) = 0;
+	*(unsigned short *)(p + 7) = 0;
+	*(unsigned short *)(p + 9) = 0;
+	*(unsigned *)(p + 11) = tex();
+	fwrite(buf, 1, sizeof(buf), fp);
+}
 
 class Sector;
 class Node;
@@ -204,18 +237,85 @@ public:
 	void add(const Wall& w) { walls_.push_back(w); }
 	SplitStats computeStats(const Plane2d& plane) const;
 
-	Sector slice(const Plane2d& plane) const;
+	Sector partition(const Plane2d& plane) const;
 	void checkWalls() const;
 	void print(std::ostream& os) const;
 	size_t countWalls() const { return walls_.size(); }
 	bool empty() const { return walls_.empty(); }
+	void collect(Vertexes& vs) const;
+	void save(FILE* fp, const Vertexes& vs) const;
 
 private:
 	void checkWallsSub() const;
+	void saveSorted(FILE* fp, const Vertexes& vs) const;
 
 	int id_;
 	Walls walls_;
 };
+
+void add(Vertexes& vs, const Vertex& v) {
+	if (std::find(vs.begin(), vs.end(), v) == vs.end()) vs.push_back(v);
+}
+
+void Sector::collect(Vertexes& vs) const {
+	for (Walls::const_iterator i(walls_.begin()); i != walls_.end(); ++i) {
+		::bsp::add(vs, i->a());
+		::bsp::add(vs, i->b());
+	}
+}
+
+template <class T>
+T pick(std::vector<T>& v, typename std::vector<T>::iterator i) {
+	T result(*i);
+	*i = v.back();
+	v.pop_back();
+	return result;
+}
+
+void Sector::saveSorted(FILE* fp, const Vertexes& vs) const {
+	assert(walls_.size() >= 3);
+	Walls ws(walls_);
+	Vertex first, last;
+	bool open = false;
+	while (!ws.empty()) {
+		assert(ws.front().a() != ws.front().b());
+		if (!open) {
+			assert(ws.size() >= 3);
+			Wall w(pick(ws, ws.begin()));
+			w.save(fp, vs);
+			first = w.a();
+			last = w.b();
+			open = true;
+			continue;
+		}
+		bool found = false;
+		for (Walls::iterator i(ws.begin()); i != ws.end(); ++i) {
+			if (i->a() == last) {
+				Wall w(pick(ws, i));
+				w.save(fp, vs);
+				last = w.b();
+				if (last == first) open = false;
+				found = true;
+				break;
+			}
+		}
+		if (!found) throw std::runtime_error("open sector");
+	}
+	if (open) throw std::runtime_error("open sector at the end");
+}
+
+void Sector::save(FILE* fp, const Vertexes& vs) const {
+	std::cerr << "Sector::save()" << std::endl;
+	{
+		int wallCount = countWalls();
+		fwrite(&wallCount, sizeof(int), 1, fp);
+	}
+	{
+		int sectorId = id();
+		fwrite(&sectorId, sizeof(int), 1, fp);
+	}
+	saveSorted(fp, vs);
+}
 
 SplitStats Sector::computeStats(const Plane2d& plane) const {
 	SplitStats result;
@@ -233,7 +333,7 @@ SplitStats Sector::computeStats(const Plane2d& plane) const {
 	return result;
 }
 
-Sector Sector::slice(const Plane2d& plane) const {
+Sector Sector::partition(const Plane2d& plane) const {
 	typedef std::map<Vertex, size_t> Degs;
 	Degs degs;
 	Sector result(id());
@@ -244,10 +344,10 @@ Sector Sector::slice(const Plane2d& plane) const {
 			const Vertex isp(intersect(plane, Plane2d(i->a(), i->b())));
 			++degs[isp];
 			if (da < 0) {
-				result.add(Wall(isp, i->b()));
+				result.add(Wall(isp, i->b(), i->tex()));
 				++degs[i->b()];
 			} else {
-				result.add(Wall(i->a(), isp));
+				result.add(Wall(i->a(), isp, i->tex()));
 				++degs[i->a()];
 			}
 		} else if (da + db > std::numeric_limits<float>::epsilon()) {
@@ -265,21 +365,13 @@ Sector Sector::slice(const Plane2d& plane) const {
 	for (Degs::const_iterator i(degs.begin()); i != degs.end(); ++i) {
 		if (!(i->second & 1)) continue;
 		if (open) {
-			result.add((plane.dot(last - i->first) < 0) ? Wall(i->first, last) : Wall(last, i->first));
+			result.add((plane.dot(last - i->first) < 0) ? Wall(i->first, last, 0) : Wall(last, i->first, 0));
 		} else {
 			last = i->first;
 		}
 		open = !open;
 	}
 	if (open) throw std::runtime_error("open sector after split");
-	return result;
-}
-
-template <class T>
-T pick(std::vector<T>& v, typename std::vector<T>::iterator i) {
-	T result(*i);
-	*i = v.back();
-	v.pop_back();
 	return result;
 }
 
@@ -359,11 +451,13 @@ public:
 	const Node& back() const{ return *back_; }
 
 	void build();
-	void save(FILE*) const {}
+	void save(FILE*, const Vertexes& vs) const;
 	void add(int sectorId, const Wall& w);
 	Plane2d plane() const { return plane_; }
 	size_t countWalls() const;
 	void print(std::ostream& os) const;
+	void collect(Vertexes& vs) const;
+	size_t countNodes() const { return 1 + (front_.get() ? front_->countNodes() : 0) + (back_.get() ? back_->countNodes() : 0); }
 
 private:
 	Node(const Node&);
@@ -378,6 +472,14 @@ private:
 	std::auto_ptr<Node> front_, back_;
 };
 
+void Node::collect(Vertexes& vs) const {
+	for (Sectors::const_iterator i(sectors_.begin()); i != sectors_.end(); ++i) {
+		i->collect(vs);
+	}
+	if (front_.get()) front_->collect(vs);
+	if (back_.get()) back_->collect(vs);
+}
+
 void
 Renderer::draw(const Node& n)
 {
@@ -385,7 +487,7 @@ Renderer::draw(const Node& n)
 	if (n.hasFront()) draw(n.front());
 	if (n.hasBack()) draw(n.back());
 	grSetColor(0xe0);
-	draw(n.plane());
+//	draw(n.plane());
 }
 
 SplitStats Node::computeStats(const Plane2d& plane) const {
@@ -432,9 +534,11 @@ Node::findBestPlane()
 void
 Node::build()
 {
-//	static int k = 0;
+/*	static int k = 0;
+	++k;
+	if (k >= 299) return;
+	std::cerr << "k=" << k << std::endl;*/
 	checkWalls();
-//	std::cerr << "k=" << k << std::endl;
 	print(std::cerr);
 	if (!findBestPlane()) return;
 	std::cerr << "best plane is:";
@@ -444,25 +548,24 @@ Node::build()
 		std::auto_ptr<Node> front(new Node);
 		std::auto_ptr<Node> back(new Node);
 		for (Sectors::const_iterator i(sectors_.begin()); i != sectors_.end(); ++i) {
-			const Sector frontSlice(i->slice(plane_));
+			const Sector frontSlice(i->partition(plane_));
 			if (!frontSlice.empty()) front->sectors_.push_back(frontSlice);
-			const Sector backSlice(i->slice(-plane_));
+			const Sector backSlice(i->partition(-plane_));
 			if (!backSlice.empty()) back->sectors_.push_back(backSlice);
 		}
 		if (!front->countWalls() || !back->countWalls()) {
 			throw std::runtime_error("ough");
 			return;
 		}
-//		if (k++ >= 9) return;
 		front_.reset(front.release());
-//		back_.reset(back.release());
+		back_.reset(back.release());
 	}
 	sectors_.clear();
 /*	grBegin();
 	Renderer().draw(*front_);
 	grEnd();*/
 	front_->build();
-//	back_->build();
+	back_->build();
 }
 
 size_t Node::countWalls() const {
@@ -470,6 +573,8 @@ size_t Node::countWalls() const {
 	for (Sectors::const_iterator i(sectors_.begin()); i != sectors_.end(); ++i) {
 		result += i->countWalls();
 	}
+	if (front_.get()) result += front_->countWalls();
+	if (back_.get()) result += back_->countWalls();
 	return result;
 }
 
@@ -480,18 +585,58 @@ void Node::print(std::ostream& os) const {
 	}
 }
 
+void Node::save(FILE* fp, const Vertexes& vs) const {
+	std::cerr << "Node::save()" << std::endl;
+	const int isLeaf = !sectors_.empty();
+	fwrite(&isLeaf, sizeof(int), 1, fp);
+	if (isLeaf) {
+		assert(!front_.get());
+		assert(!back_.get());
+		assert(sectors_.size() == 1);
+		sectors_.front().save(fp, vs);
+	} else {
+		assert(front_.get());
+		assert(back_.get());
+		assert(sectors_.empty());
+		plane_.save(fp);
+		front_->save(fp, vs);
+		back_->save(fp, vs);
+	}
+}
+
 class Tree {
 public:
 	void clear() { root_.reset(new Node); }
 	void build() { try{if (root_.get()) root_->build();}catch(...){} }
 //	void build() { if (root_.get()) root_->build(); }
 	void show() const { if (root_.get()) Renderer().draw(*root_); }
-	void save(FILE* fp) const { if (root_.get()) root_->save(fp); }
+	void save(FILE* fp) const;
 	void add(int sectorId, const Wall& w) { if (root_.get()) root_->add(sectorId, w); }
 
 private:
 	std::auto_ptr<Node> root_;
 };
+
+void Tree::save(FILE* fp) const {
+	assert(root_.get());
+	Vertexes vertexes;
+	root_->collect(vertexes);
+	{
+		unsigned vertexCount = vertexes.size();
+		std::cerr << vertexCount << " vertexes" << std::endl;
+		fwrite(&vertexCount, sizeof(unsigned), 1, fp);
+		for (Vertexes::const_iterator i(vertexes.begin()); i != vertexes.end(); ++i) {
+			::bsp::save(fp, *i);
+		}
+	}
+	unsigned nodeCount = root_->countNodes();
+	std::cerr << nodeCount << " nodes" << std::endl;
+	unsigned lineCount = root_->countWalls();
+	std::cerr << lineCount << " lines" << std::endl;
+	fwrite(&nodeCount, sizeof(unsigned), 1, fp);
+	fwrite(&lineCount, sizeof(unsigned), 1, fp);
+	root_->save(fp, vertexes);
+}
 
 namespace { Tree tree; }
 
@@ -502,9 +647,9 @@ void bspShow() { tree.show(); }
 int bspSave(FILE* f) { tree.save(f); return 0; }
 
 int
-bspAddLine(int s, int x1, int y1, int x2, int y2, int, int, int, int, int)
+bspAddLine(int s, int x1, int y1, int x2, int y2, int, int, int, int tex, int)
 {
-	tree.add(s, Wall(Vertex(x1, y1), Vertex(x2, y2)));
+	tree.add(s, Wall(Vertex(x1, y1), Vertex(x2, y2), tex));
 	return 0;
 }
 
